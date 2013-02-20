de400a92e9c5a251438e06c849c32478c34f0bc5
ankurcompro <ankur.sharma@comprotechnologies.com>
2013-02-20 00:31:20 -0800

    Updating xml

--- src/testautomation/testcases/PearsonHSCTests.java
+++ src/testautomation/testcases/PearsonHSCTests.java
@@ -16,7 +16,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
-import org.openqa.jetty.jetty.servlet.AbstractSessionManager.Session;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
@@ -28,14 +27,6 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import testautomation.framework.AssertionTest;
 import testautomation.framework.LogGenerator;
 import testautomation.framework.SetupDriver;
-
-import javax.mail.*;
-import javax.mail.internet.*;
-import javax.mail.Authenticator;
-import javax.mail.PasswordAuthentication;
-import java.util.Properties;
-
-
 @RunWith(Parameterized.class)
 public class PearsonHSCTests {
 	public static int index=0;
@@ -141,10 +132,10 @@ public class PearsonHSCTests {
 		WebElement ClickPlayAll = driver.findElement(By.xpath("//button[@class='btn playAll']"));
 		Boolean playalldisplayed=driver.findElement(By.xpath("//button[@class='btn playAll']")).isEnabled();
 		System.out.println("Is Play All Button Enabled="+playalldisplayed);
-
+		
 		//ClickPlayAll.click();
 		((JavascriptExecutor) driver).executeScript("arguments[0].click();", ClickPlayAll);
-
+		
 		String buttonstate = "NotPlayed";
 				while(!buttonstate.equals("Played"))
 				{
@@ -333,7 +324,7 @@ public class PearsonHSCTests {
 
 		//Click on Logout Button
 		driver.findElement(By.xpath("//*[@id=\"logout-button\"]")).click();
-
+		
 		// Waiting for user to get Logged out.
 					String logout = "Not_logged_out";
 					while(!logout.equals("logged_out"))
@@ -357,9 +348,9 @@ public class PearsonHSCTests {
 
 //		synchronized (driver) {
 //			driver.wait(7000);}
-
-
-
+		
+				
+		
 		//*******************************************************************************************************
 		//********************************************************End of Test 1***********************************************
 		}
@@ -705,10 +696,10 @@ public class PearsonHSCTests {
 					System.out.println("Waiting for user to get logged out");
 				}
 	}
-
+			
 //			synchronized (driver) {
 //				driver.wait(10000);}
-
+			
 			//********************************************************************************
 		//***********************************End of Facebook Test***********************************
 		}
@@ -731,68 +722,10 @@ public class PearsonHSCTests {
 	              e.printStackTrace();
 	         }
 	         */
-
-
 	 }
 
-public class SimpleMail {
-
-    private static final String SMTP_HOST_NAME = "smtp.sendgrid.net";
-    private static final String SMTP_AUTH_USER = "cloudbees_compro";
-    private static final String SMTP_AUTH_PWD  = "compro123";
-
-    public void main(String[] args) throws Exception{
-       new SimpleMail().test();
-    }
-
-    public void test() throws Exception{
-        Properties props = new Properties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.host", SMTP_HOST_NAME);
-        props.put("mail.smtp.port", 587);
-        props.put("mail.smtp.auth", "true");
-
-        Authenticator auth = new SMTPAuthenticator();
-        Session mailSession = Session.getDefaultInstance(props, auth);
-        // uncomment for debugging infos to stdout
-        // mailSession.setDebug(true);
-        Transport transport = mailSession.getTransport();
-
-        MimeMessage message = new MimeMessage(mailSession);
-
-        Multipart multipart = new MimeMultipart("alternative");
-
-        BodyPart part1 = new MimeBodyPart();
-        part1.setText("This is multipart mail and u read part1......");
-
-        BodyPart part2 = new MimeBodyPart();
-        part2.setContent("<b>This is multipart mail and u read part2......</b>", "text/html");
-
-        multipart.addBodyPart(part1);
-        multipart.addBodyPart(part2);
-
-        message.setContent(multipart);
-        message.setFrom(new InternetAddress("me@myhost.com"));
-        message.setSubject("This is the subject");
-        message.addRecipient(Message.RecipientType.TO,
-             new InternetAddress("ankur.sharma@comprotechnologies.com"));
-
-        transport.connect();
-        transport.sendMessage(message,
-            message.getRecipients(Message.RecipientType.TO));
-        transport.close();
-    }
-    private class SMTPAuthenticator extends javax.mail.Authenticator {
-        public PasswordAuthentication getPasswordAuthentication() {
-           String username = SMTP_AUTH_USER;
-           String password = SMTP_AUTH_PWD;
-           return new PasswordAuthentication(username, password);
-        }
-    }
-
-
 }
-}
+
 
 
 
